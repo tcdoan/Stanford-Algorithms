70c5b31dcc1e75e5c428515dd1cee7f104162c0e
Thanh Doan <thdoa@microsoft.com>
2018-04-08 15:59:07 -0700

    Dynamic programming implementation of the knapsack problem.

---
+++ course3/w4/Knapsack/item.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Item
+{
+	int value;
+	int weight;
+	Item(int v, int w) : value(v), weight(w) {}
+};
---
+++ course3/w4/Knapsack/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include "item.h"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Implement knapsack algorithm and run it on knapsack1.txt data set
+int main()
+{
+	ifstream ifs("C:\\repos\\algorithm\\course3\\w4\\knapsack1.txt");
+
+	int capcity, N;
+	ifs >> capcity;
+	ifs >> N;
+
+	vector<Item> items;
+	for (int i = 0; i < N; i++)
+	{
+		int value, weight;
+		ifs >> value;
+		ifs >> weight;
+		Item item(value, weight);
+		items.emplace_back(item);
+	}
+
+	// solution stored in 2-d array a
+	vector<vector<int64_t>> a(N+1);
+	for (int i = 0; i <= N; i++)
+	{
+		a[i] = vector<int64_t>(capcity + 1);
+		a[i][0] = 0;
+	}
+
+	for (int x = 0; x <= capcity; x++)
+	{
+		a[0][x] = 0;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int x = 0; x <= capcity; x++)
+		{
+			if (x - items[i].weight >= 0)
+			{
+				a[i][x] = max(a[i - 1][x], a[i-1][x-items[i].weight] + items[i].value);
+			}
+			else
+			{
+				a[i][x] = a[i - 1][x];
+			}
+		}
+	}
+
+	cout << "Optimal value: " << a[N][capcity] <<endl;
+}
+
